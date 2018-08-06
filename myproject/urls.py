0a3908cb33dbe6c9d0142237eca05289543c6886
stranger1989 <stranger1989@example.com>
2018-08-06 22:19:11 +0000

    Stranger app

--- myproject/urls.py
+++ myproject/urls.py
@@ -20,3 +20,9 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
 
+import settings
+urlpatterns += patterns('',
+    url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    }),
+)
