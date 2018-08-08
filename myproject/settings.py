5437d0b0fe97601589e7ff3da81b4d3745a874f3
stranger1989 <stranger1989@example.com>
2018-08-08 21:04:13 +0000

    working application

--- myproject/settings.py
+++ myproject/settings.py
@@ -119,12 +119,12 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+#STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+#STATICFILES_DIRS = (
+#    os.path.join(BASE_DIR, 'static'),
+#)
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config()
