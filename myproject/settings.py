03d43327de88e6485aad904a5e0050a4ff28697f
stranger1989 <stranger1989@example.com>
2018-08-09 21:56:30 +0000

    make staticfiles

--- myproject/settings.py
+++ myproject/settings.py
@@ -119,11 +119,11 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 import dj_database_url
