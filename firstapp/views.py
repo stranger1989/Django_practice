cf503cf5f79c3e2d39abc48bd78ae6c09ccd2043
stranger1989 <stranger1989@example.com>
2018-08-14 01:34:01 +0000

    fill the django code in the html templates

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
 def post_list(request):
-    return render(request, 'firstapp/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'firstapp/post_list.html', {'posts':posts})
 
